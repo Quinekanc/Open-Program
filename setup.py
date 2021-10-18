4775dd801b968bd8419caa1b96ae9de91eeafc8b
AyfDROID <sergeyzargatifg@gmail.com>
2021-10-18 12:02:58 +0300

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='OP',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Quienaknc',
+    author_email='',
+    description=''
+)
